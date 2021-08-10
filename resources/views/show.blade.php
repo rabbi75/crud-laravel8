f6455b0d9a4dbdfe89fa2ca6a2b1540ad2d09d7f
Golam Rabbi <spirabbi@gmail.com>
2021-08-10 11:15:43 +0600

    fixed readonly

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,19 +4,19 @@
         <form action="{{ route('information.welcome') }}">
             <div class="form-group">
                 <label for="exampleInputEmail1">Name</label>
-                <input type="text" name="name" value="{{$information->name}}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
+                <input type="text" name="name" value="{{$information->name}}" readonly class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
             </div>
             <div class="form-group">
                 <label for="exampleInputEmail1">Email</label>
-                <input type="text" name="email" value="{{$information->email}}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="text" name="email" value="{{$information->email}}" readonly class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
             </div>
             <div class="form-group">
                 <label for="exampleInputEmail1">Description</label>
-                <textarea type="text" name="description" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">{{$information->description}}</textarea>
+                <textarea type="text" name="description" readonly class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">{{$information->description}}</textarea>
             </div>
             <div class="form-group">
                 <label for="oldimage">Image</label></br>
-                <img src="{!! asset('images/'. $information->image) !!}" width="100" height="100"></br>
+                <img src="{!! asset('images/'. $information->image) !!}" readonly width="100" height="100"></br>
                 </div>
             <button type="submit" class="btn btn-primary mt-2">OK</button>
         </form>
